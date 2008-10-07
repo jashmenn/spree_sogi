59103a098ddbf86a027cabe8f776cae6405eb369
Nate Murray <nate@natemurray.com>
2008-10-07 11:20:20 -0700

    wrote custom order data wrong

--- app/models/sogi/order_parser.rb
+++ app/models/sogi/order_parser.rb
@@ -19,14 +19,40 @@ class Sogi::OrderParser
   class Order < OpenStruct
     attr_accessor :document
     include XmlParsingHelperMethods
+
+    class << self
+      def add_custom_data(key, xpath)
+        @@custom_data ||= {}
+        @@custom_data[key] = xpath
+
+        define_method key do
+          read_custom_data(key)
+        end
+      end
+    end
+
+    def initialize
+      super
+    end
+
+    def read_custom_data(key)
+      v(@@custom_data[key])
+    end
+    protected :read_custom_data
+
+    def custom_data
+      @@custom_data ||= {}
+    end
   end
 
   attr_accessor :body
   attr_accessor :document
+  include XmlParsingHelperMethods
 
   class << self
 
     def custom_order_data(key, xpath)
+      Order.add_custom_data(key, xpath)
     end
 
     def define_order_methods_as(&block)
--- spec/models/sogi/parser/amazon_spec.rb
+++ spec/models/sogi/parser/amazon_spec.rb
@@ -6,18 +6,23 @@ describe Sogi::Parser::Amazon do
     @parser.body = File.read(SOGI_FIXTURES_PATH + "/sample_xml/amazon_order_sample.xml")
     @parser.body.should_not be_nil
     @parser.document.should_not be_nil
+    @order = @parser.orders[0]
   end
 
   it "should know about various orders xml orders" do
     @parser.orders.should_not be_nil
     @parser.orders.size.should eql(2)
-    order = @parser.orders[0]
-    order.should_not be_nil
-    order.should respond_to(:order_id)
-    order.order_id.should eql("050-1234567-1234567")
+    @order.should_not be_nil
+    @order.should respond_to(:order_id)
   end
 
   it "should know about various order numbers" do
+    @order.order_id.should eql("050-1234567-1234567")
+  end
+
+  it "should have custom order data and know the merchant id" do
+    @order.custom_data.should have_at_least(1).items
+    @order.amazon_merchant_identifier.should eql("My Store")
   end
 
   it "should have information about dates" do
